e20c552ee82cb061d9ccd1aeaeae0d6cd5627f33
GetecOffice <getec.plc@gmail.com>
2025-07-08 01:23:20 -0600

    MONITOREO

--- Aplicacion/monitoreo.py
+++ Aplicacion/monitoreo.py
@@ -12,9 +12,9 @@ from Aplicacion.models import Registro
 
 def monitorear():
     while True:
-        registro = Registro.objects.using('servidor').filter(estatus=3).first()
+        registro = Registro.objects.using('servidor').filter(estatus=2).first()
         if registro:
-            registro.estatus = 4
+            registro.estatus = 1
             registro.save(using='servidor')
             print(f"✔ Actualizado: {registro.descripcion}")
         else:
